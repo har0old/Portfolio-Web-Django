5b609b24cf4519fa7bdf3ead0efb332d3e1b64d8
har0old <garciavargas9112@gmail.com>
2022-02-27 23:55:56 -0500

    añadiendo proyecto

---
+++ webpersonal/portfolio/apps.py
@@ -0,0 +1,8 @@
+from tabnanny import verbose
+from django.apps import AppConfig
+
+
+class PortfolioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'portfolio'
+    verbose_name='portafolio'
